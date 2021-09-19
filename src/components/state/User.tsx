6a6583c3cafc387b04a5c107031316eaac4a384e
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-09-19 22:40:55 +0100

    type assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,7 +6,7 @@ type AuthUser = {
 }
 
 export const User = () => {
-    const [user, setUser] = useState<AuthUser | null>(null)
+    const [user, setUser] = useState<AuthUser>({} as AuthUser)
 
     const handleLogin = () => {
         setUser({
@@ -14,16 +14,16 @@ export const User = () => {
             email: 'samsolo@gmail.com',
         })
     }
-    const handleLogout = () => {
-        setUser(null)
-    }
+    // const handleLogout = () => {
+    //     setUser(null)
+    // }
 
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.email}</div>
+            {/* <button onClick={handleLogout}>Logout</button> */}
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
         </div>
     )
 }
